e358a5c5345853ab9390783598517300f93f87ef
Huzaifa Moin <huzaifamoin784@gmail.com>
2026-08-08 23:21:49 +0500

    Create Builder Page and AppContext Page

---
+++ client/src/components/BuilderHeader.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { ArrowLeftIcon, EyeIcon, Code2Icon, ExternalLinkIcon, Loader2Icon, GlobeIcon, DownloadIcon  } from 'lucide-react'
+
+const BuilderHeader = ({
+    projectName,
+    version,
+    showCode,
+    publishing,
+    onToggleShowCode,
+    onOpenPreview,
+    onPublish,
+    onDownload,
+    onBack,
+    onLogout,
+}) => {
+  return (
+<header className="h-12 shrink-0 flex items-center justify-between px-3 border-b border-zinc-400 bg-white">
+        <div className="flex items-center gap-2">
+            <button onClick={onBack} className='p-1.5 rounded-md text-zinc-600 hover:text-zinc-950 hover:bg-zinc-100 cursor-pointer'>
+                <ArrowLeftIcon size={18} />
+            </button>
+            <img src="/logo.svg" alt="" className="invert size-5" />
+            <span className="text-sm font-semibold truncate max-w-38 md:max-w-50">
+            {projectName}</span>
+            <span className="text-[10px] px-1.5 py-0.5 rounded bg-zinc-100 text-zinc-500 font-medium">v{version}</span>
+        </div>
+
+        <div className="flex items-center gap-1.5">
+            <button onClick={onToggleShowCode}
+            className={`inline-flex items-center justify-center gap-1.5 py-1.5 px-3 border border-zinc-300 text-zinc-800 hover:bg-zinc-100 hover:text-zinc-900 text-s font-medium rounded-lg cursor-pointer bg-white ${showCode ? "bg-zinc-100 text-zinc-900" : ""}`}>
+                {showCode ? (
+                    <>
+                    <EyeIcon size={18}/> Preview
+                    </>
+                ) : (
+                    <>
+                    <Code2Icon size={18}/> Code
+                    </>
+                )}
+            </button>
+
+            <button onClick={onOpenPreview}
+            className='inline-flex items-center justify-center gap-1.5 py-1.5 px-3 border border-zinc-300 text-zinc-800 hover:bg-zinc-100 hover:text-zinc-900 text-s font-medium rounded-lg cursor-pointer bg-white'>
+                <ExternalLinkIcon size={18} /> Open Preview
+            </button>
+
+            <button onClick={onPublish} disabled={publishing}
+            className='inline-flex items-center justify-center gap-1.5 py-1.5 px-3 border border-zinc-300 text-zinc-800 hover:bg-zinc-100 hover:text-zinc-900 text-s font-medium rounded-lg cursor-pointer bg-white'>
+                {publishing ? <Loader2Icon size={18} className="animate-spin"/> : <GlobeIcon size={18}/>} Publish
+            </button>
+
+            <button onClick={onDownload}
+            className='inline-flex items-center justify-center gap-1.5 py-1.5 px-3 border border-zinc-300 text-zinc-800 hover:bg-zinc-100 hover:text-zinc-900 text-s font-medium rounded-lg cursor-pointer bg-white'>
+                <DownloadIcon size={18} /> Export
+            </button>
+             <button onClick={onLogout}
+            className='inline-flex items-center justify-center gap-1.5 py-1.5 px-3 border border-zinc-400 text-zinc-800 hover:bg-zinc-100 hover:text-zinc-900 text-s font-medium rounded-lg cursor-pointer bg-white'>
+                Sign Out
+            </button>
+        </div>
+
+    </header> 
+     )
+}
+
+export default BuilderHeader
--- client/src/pages/BuilderPage.jsx
+++ client/src/pages/BuilderPage.jsx
@@ -1,8 +1,96 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useAppContext } from '../context/AppContext'
+import Loading from '../components/Loading'
+import BuilderHeader from '../components/BuilderHeader'
+import { MessageSquareIcon, FolderTreeIcon } from 'lucide-react'
+import ChatPanel from '../components/ChatPanel'
 
 const BuilderPage = () => {
+
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const [leftTab, setLeftTab] = useState("chat");
+  const [publishing, setPublishing] = useState(false);
+  const [publishUrl, setPublishUrl] = useState(null);
+
+  const { activeProject, loadingActiveProject, activeFile, showCode, setActiveFile,
+    setShowCode, loadProject, logout, chatLoading, handleChat } = useAppContext();
+
+ 
+
+  useEffect(() => {
+    if (!id) return;
+    loadProject(id)
+  }, [id])
+
+  useEffect(() => {
+    if (!id || !activeProject) return;
+    if (activeProject.status === "pending" || activeProject.status === "generating") {
+      const interval = setInterval(() => {
+        loadProject(id, true)
+      }, 1500)
+      return () => clearInterval(interval)
+    }
+  }, [id, loadProject, activeProject])
+
+  const handleOpenPreview = () => {
+    if (!id) {
+      return;
+    }
+    window.open(`/preview/${id}`, "_blank")
+  }
+
+  const handlePublish = async () => {
+
+  }
+
+  const handleDownload = async () => {
+
+  }
+
+
+  if (loadingActiveProject || !activeProject) {
+    return <Loading />
+  }
+
   return (
-    <div>BuilderPage</div>
+    <div className="h-screen flex flex-col bg-white overflow-hidden text-zinc-900 relative">
+      {/* Top Bar Header */}
+      <BuilderHeader projectName={activeProject.name} version={activeProject.version} showCode={showCode} publishing={publishing} onToggleShowCode={() => setShowCode(!showCode)} onOpenPreview={handleOpenPreview} onPublish={handlePublish} onDownload={handleDownload} onBack={() => navigate('/')} onLogout={logout} />
+      {/* Main Layout */}
+      <div className="flex-1 flex overflow-hidden">
+        {/* Left Sidebar */}
+        <div className="w-[320px] shrink-0 flex flex-col border-r border-zinc-200 bg-white">
+          {/* Sidebar Tabs */}
+          <div className="flex border-b border-zinc-100">
+            <button onClick={() => setLeftTab("chat")}
+              className={`flex-1 flex items-center justify-center gap-1.5 py-2.5 text-s font-medium cursor-pointer ${leftTab === "chat" ? "text-zinc-900 border-b-2 border-zinc-900" : "text-zinc-600 hover:text-zinc-700"}`}>
+              <MessageSquareIcon size={18} /> Chat
+            </button>
+
+            <button onClick={() => setLeftTab("files")}
+              className={`flex-1 flex items-center justify-center gap-1.5 py-2.5 text-s font-medium cursor-pointer ${leftTab === "files" ? "text-zinc-900 border-b-2 border-zinc-900" : "text-zinc-600 hover:text-zinc-700"}`}>
+              <FolderTreeIcon size={18} /> Files
+            </button>
+          </div>
+          {/* Sidebar Content */}
+          <div className="flex-1 overflow-hidden">
+            {
+              leftTab === 'chat' ? (
+                <ChatPanel messages={activeProject.messages} onSend={handleChat} loading=
+                      {chatLoading}/>
+                    ) : (
+                <div>FileExplorer</div>
+              )
+            }
+          </div>
+        </div>
+
+        {/* Preview / Code Area */}
+
+      </div>
+    </div>
   )
 }
 
