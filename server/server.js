1203b7e5587733ab8f5e16df9833fdb5d40cb8ac
Huzaifa Moin <huzaifamoin784@gmail.com>
2026-08-10 19:56:24 +0500

    Create Backend server

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { connectToDatabase } from "./config/db.js";
+
+const app = express();
+
+connectToDatabase()
+
+app.use(cors({origin: process.env.ORIGINS.split(","), credentials: true}))
+app.use(cookieParser())
+app.use(express.json())
+
+app.get("/", (req, res)=> res.send("Server is Live!"))
+
+// Centralized error handler
+app.use((err, _req, res, _next)=>{
+    console.error(`[Error] ${err.message}`);
+    res.status(500).json({error: err.message})
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, ()=>{
+    console.log(`server is running on port: ${port}`)
+})
