5048026d910ac6324af6684fc3daa06655f026fa
Huzaifa Moin <huzaifamoin784@gmail.com>
2026-08-23 11:28:29 +0500

    Project complete

--- client/src/components/FullPagePreview.jsx
+++ client/src/components/FullPagePreview.jsx
@@ -34,6 +34,7 @@ const FullPagePreview = ({files}) => {
                 customSetup={{dependencies}}
                 options={{
                     externalResources: [
+                        "https://cdn.tailwindcss.com",
                         "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
                     ],
                     logLevel: 0,
